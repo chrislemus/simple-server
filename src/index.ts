1b89d3633b1a1e93c7aa9daebd970dc6278ce126
chrislemus <icrislemus@gmail.com>
2021-08-10 15:32:59 -0400

    create LoginController class to link typescript decorators

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import { router } from './routes/loginRoutes'
 import cookieSession from 'cookie-session'
-
+import { AppRouter } from './AppRouter'
+import './controllers/LoginController'
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(cookieSession({keys: ['secret']}))
 app.use(router)
+app.use(AppRouter.getInstance())
 
 app.listen(3000, () => console.log('listening on port 3000'))
